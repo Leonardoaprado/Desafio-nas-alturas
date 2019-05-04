0a7acfb4ac0c54a3f3ec315dde87dd3dfbbc484a
LEONARDO AUGUSTO DO PRADO <leonardo.aprado@senacsp.edu.br>
2019-05-03 22:00:50 -0300

    adicionando fisica

---
+++ DesafioNasAlturas/Assets/Script/aviao.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aviao : MonoBehaviour
+{
+    Rigidbody2D fisica;
+
+    
+    private void Awake()
+    {
+        //Debug.Log("Acordei");
+        this.fisica = this.GetComponent<Rigidbody2D>();
+    }
+    
+    private void impulsionar()
+    {
+        this.fisica.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            //Debug.Log("clicou");
+            impulsionar();
+        }
+    }
+}
