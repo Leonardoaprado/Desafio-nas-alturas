35a9227dbfe57cba15e2a44b274f9ae35f0042cd
LEONARDO AUGUSTO DO PRADO <leonardo.aprado@senacsp.edu.br>
2019-05-10 20:00:39 -0300

    gerando obstaculos infinitos

---
+++ DesafioNasAlturas/Assets/Script/GeradorDeObstaculo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorDeObstaculo : MonoBehaviour
+{
+
+    [SerializeField]
+    private float tempoParaGerar = 3f;
+
+    [SerializeField]
+    private GameObject manualDeInstrucoes;
+
+    private float cronometro = 3f;
+
+    void Awake()
+    {
+        cronometro = tempoParaGerar;
+    }
+
+
+    void Update()
+    {
+        cronometro -= Time.deltaTime;
+
+        if(cronometro <= 0)
+        {
+            //Debug.Log("3 segundo");
+            // Debug.Log(Quaternion.identity);
+            GameObject.Instantiate(manualDeInstrucoes, transform.position, Quaternion.identity);
+            cronometro = tempoParaGerar;
+        }
+    }
+}
